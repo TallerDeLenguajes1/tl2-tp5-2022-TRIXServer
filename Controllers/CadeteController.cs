c893ddb0ac9383a0c7f9f3c5a3713d0b9678ca58
TRIXServer <admin@trixserver.com>
2022-11-11 12:20:05 -0300

    commit

--- Controllers/CadeteController.cs
+++ Controllers/CadeteController.cs
@@ -38,6 +38,7 @@ namespace tl2_tp5_2022_TRIXServer.Controllers
             {
                 Database.Cadetes.Add(new CadeteViewModel(Database.Id_cadete, dataCadete.Nombre, dataCadete.Telefono, dataCadete.Direccion));
                 Database.Id_cadete++;
+
                 return RedirectToAction("Index");
 
             }
@@ -48,6 +49,44 @@ namespace tl2_tp5_2022_TRIXServer.Controllers
             }
             
         }
+
+        public IActionResult Edit(int dataId)
+        {
+            var elCadete = Database.Cadetes.Find(search => search.Id_persona == dataId);
+            return View(new EditCadeteViewModel(dataId, elCadete.Nombre, elCadete.Telefono, elCadete.Direccion));
+
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(EditCadeteViewModel dataCadete)
+        {
+            if (ModelState.IsValid)
+            {
+                var elCadete = Database.Cadetes.Find(search => search.Id_persona == dataCadete.Id_persona);
+                elCadete.Nombre = dataCadete.Nombre;
+                elCadete.Telefono = dataCadete.Telefono;
+                elCadete.Direccion = dataCadete.Direccion;
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+
+            }
+
+        }
+
+        public IActionResult Baja(int dataId)
+        {
+            var elCadete = Database.Cadetes.Find(search => search.Id_persona == dataId);
+            Database.Cadetes.Remove(elCadete);
+
+            return RedirectToAction("Index");
+            
+        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
